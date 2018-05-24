2744f006fb5440e2146deb81af436ba8e8ce78fe
Jake <jakemacnaughton@gmail.com>
2018-05-24 14:31:21 -0400

    removed solutions

--- models.py
+++ models.py
@@ -5,32 +5,10 @@ from sqlalchemy.orm import sessionmaker, relationship, backref
 Base = declarative_base()
 
 # Write your classes below
-class Artist(Base):
-    __tablename__ = 'artists'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    genres = relationship(
-        'Genre',
-        secondary='songs'
-    )
 
-class Genre(Base):
-    __tablename__ = 'genres'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    artists = relationship(
-        Artist,
-        secondary='songs'
-    )
 
-class Song(Base):
-    __tablename__ = 'songs'
-    id = Column(Integer, primary_key=True)
-    artist_id = Column(Integer, ForeignKey('artists.id'))
-    genre_id = Column(Integer, ForeignKey('genres.id'))
-    name = Column(String)
-    artist = relationship(Artist, backref=backref("songs", cascade="all, delete-orphan"))
-    genre = relationship(Genre, backref=backref("songs", cascade="all, delete-orphan"))
+
+
 
 
 engine = create_engine('sqlite:///:memory:')
